ec25def18f51d3e2d3dd6863b8123ff8ef4646a0
Shelwin <shelwinsunga@gmail.com>
2024-08-19 20:25:57 -0500

    button render

--- components/latex-render/latex.tsx
+++ components/latex-render/latex.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
+import { Button } from '@/components/ui/button';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -17,16 +18,13 @@ export default function LatexRenderer({ latex }: LatexRendererProps) {
     const [numPages, setNumPages] = useState<number | null>(null);
     const [pdfUrl, setPdfUrl] = useState<string | null>(null);
     
-    useEffect(() => {
-        const fetchPdf = async () => {
-            const response = await fetch('http://127.0.0.1:8000');
-            const blob = await response.blob();
-            const url = URL.createObjectURL(blob);
+    const fetchPdf = async () => {
+        const response = await fetch('http://127.0.0.1:8000');
+        const blob = await response.blob();
+        const url = URL.createObjectURL(blob);
 
-            setPdfUrl(url); 
-        }
-        fetchPdf();
-    }, []);
+        setPdfUrl(url); 
+    }
 
     function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
         setNumPages(numPages);
@@ -35,6 +33,9 @@ export default function LatexRenderer({ latex }: LatexRendererProps) {
     return (
         <div className="w-full h-full">
             <h1>Latex Renderer</h1>
+            <Button onClick={fetchPdf}>
+                Generate PDF
+            </Button>
             {pdfUrl && (
                 <Document
                     file={pdfUrl}
