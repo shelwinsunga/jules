b5168f5c2d5c85d0b88e857141a8063a2e926421
Shelwin <shelwinsunga@gmail.com>
2024-08-22 17:18:54 -0500

    feat: comments

--- components/editor/hooks/useAIAssist.js
+++ components/editor/hooks/useAIAssist.js
@@ -9,8 +9,8 @@ import { applyEdit } from '../utils/applyEdit';
 
 export const useAIAssist = (editorRef) => {
 
-
     const handleAIAssist = (editor, monaco) => {
+        // Add a command to the editor that triggers on Ctrl+K
         editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyK, async () => {
             const selection = editor.getSelection();
             const initialText = editor.getModel().getValue();
@@ -22,7 +22,11 @@ export const useAIAssist = (editorRef) => {
             );
             const oldText = editor.getModel().getValueInRange(range);
             const context = `Replace lines ${selection.startLineNumber}-${selection.endLineNumber}:\n${oldText}`;
+            
+            // Prompt the user for input
             const userInput = await promptModal(editor, monaco, selection);
+            
+            // Generate new content based on the user input and context
             const { output } = await generate(`File content:\n${initialText}\n\nContext: ${context}\n\nUser input: ${userInput}`);
 
             let newText = '';
@@ -30,6 +34,7 @@ export const useAIAssist = (editorRef) => {
             let currentLine = selection.startLineNumber; 
             let buffer = '';
 
+            // Read the generated output in a streamable manner
             for await (const delta of readStreamableValue(output)) {
                 buffer += delta;
 
@@ -43,9 +48,11 @@ export const useAIAssist = (editorRef) => {
                 }
             }
 
+            // Final calculation of differences
             const { currentLine: finalLine } = calculateDiff(oldText, newText, monaco, selection);
             currentLine = finalLine;
 
+            // Create and add a content widget to the editor
             const contentWidget = createContentWidget(editor, monaco, selection, oldText, newText, currentLine, oldDecorations);
             editor.addContentWidget(contentWidget);
             
