b3181ee04a009cbca84237d7a8b5fd6aff4355f0
Shelwin <shelwinsunga@gmail.com>
2024-08-23 06:25:34 -0500

    update to 3.5 sonnet

--- app/actions.tsx
+++ app/actions.tsx
@@ -3,13 +3,13 @@
 import { streamText } from 'ai';
 import { openai } from '@ai-sdk/openai';
 import { createStreamableValue } from 'ai/rsc';
-
+import { anthropic } from '@ai-sdk/anthropic';
 export async function generate(input: string) {
   const stream = createStreamableValue({content: '', isComplete: false});
 
   (async () => {
     const { textStream } = await streamText({
-      model: openai('gpt-4o'),
+      model: anthropic('claude-3-5-sonnet-20240620'),
       system: 'Imagine you are embedded in a latex editor. You only write latex. You do not write anything else or converse; you only write latex.'
       + 'Do not write in backticks like this: ```latex ...```. Write your latex directly. You must consider the context of where you\'re starting from and ending from. For example, if you start in the middle of the document, would not write \documentclass{article} or \begin{document} and add packages. You must consider the context of where you\'re starting from and ending from.',
       prompt: input,
