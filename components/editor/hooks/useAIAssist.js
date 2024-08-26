7d650b0c73b30f82874460cab5aad8b7c216844e
Shelwin <shelwinsunga@gmail.com>
2024-08-26 16:59:00 -0500

    remove comment

--- components/editor/hooks/useAIAssist.js
+++ components/editor/hooks/useAIAssist.js
@@ -10,7 +10,6 @@ import { applyEdit } from '../utils/applyEdit';
 export const useAIAssist = (editorRef) => {
 
     const handleAIAssist = (editor, monaco) => {
-        // Add a command to the editor that triggers on Ctrl+K
         editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyK, async () => {
             const selection = editor.getSelection();
             const initialText = editor.getModel().getValue();
@@ -23,10 +22,8 @@ export const useAIAssist = (editorRef) => {
             const oldText = editor.getModel().getValueInRange(range);
             const context = `Replace lines ${selection.startLineNumber}-${selection.endLineNumber}:\n${oldText}`;
             
-            // Prompt the user for input
             const userInput = await promptModal(editor, monaco, selection);
             
-            // Generate new content based on the user input and context
             const { output } = await generate(`File content:\n${initialText}\n\nContext: ${context}\n\nUser input: ${userInput}`);
 
             let newText = '';
@@ -34,7 +31,6 @@ export const useAIAssist = (editorRef) => {
             let currentLine = selection.startLineNumber; 
             let buffer = '';
 
-            // Read the generated output in a streamable manner
             for await (const delta of readStreamableValue(output)) {
                 buffer += delta.content;
                 if (buffer.endsWith('\n') || buffer.length > 0) {
@@ -47,7 +43,6 @@ export const useAIAssist = (editorRef) => {
                 }
             }
 
-            // Create and add a content widget to the editor
             const contentWidget = createContentWidget(editor, monaco, selection, oldText, newText, currentLine, oldDecorations);
             editor.addContentWidget(contentWidget);
             
