e5360fc7e00552b1e204599d54f8b17340028621
Shelwin <shelwinsunga@gmail.com>
2024-08-26 12:21:07 -0500

    remove comments

--- components/editor/utils/applyEdit.js
+++ components/editor/utils/applyEdit.js
@@ -1,10 +1,7 @@
 'use client';
 export const applyEdit = async (editor, initialText, range, diffText) => {
-    // Reset to initial text without adding to undo stack
     editor.getModel().setValue(initialText);
-    
-    // Apply diff text without adding to undo stack
-    editor.getModel().pushEditOperations(
+        editor.getModel().pushEditOperations(
         [],
         [{
             range: range,
