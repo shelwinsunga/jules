284f43ec2f2a32b6c169fe04f7dfd8f3844a1b48
Shelwin <shelwinsunga@gmail.com>
2024-08-26 21:50:32 -0500

    rework layout

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -8,7 +8,6 @@ export default function ProjectLayout({
 }>) {
   return (
     <>
-      <Nav />
       {children}
     </>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,21 +1,25 @@
 import EditorContainer from "@/components/editor/editor-container";
 import LatexRenderer from "@/components/latex-render/latex";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
 
 export const maxDuration = 30;
 
 export default function Home() {
   return (
-    <main className="flex flex-col px-4">
-      <div className="flex flex-row justify-between">
-      </div>
-      <div className="flex flex-row space-x-4 h-[calc(100vh-100px)] rounded-md shadow-md">
-        <div className="w-1/2 flex flex-col border justify-start px-0 rounded-md bg-secondary/20 items-center">
+    <main className="flex flex-col h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel defaultSize={50}>
           <EditorContainer />
-        </div>
-        <div className="w-1/2 flex flex-col border justify-start px-0 rounded-md bg-secondary/20 items-center">
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={50}>
           <LatexRenderer />
-        </div>
-      </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </main>
   );
 }
