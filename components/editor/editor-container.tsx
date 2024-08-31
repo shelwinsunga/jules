c3984bce8145979d31e00956f2bc3660c8a668c9
Shelwin <shelwinsunga@gmail.com>
2024-08-31 14:40:44 -0500

    persisting file changes

--- components/editor/editor-container.tsx
+++ components/editor/editor-container.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import { CodeEditor } from './editor'
 import { useFrontend } from '@/contexts/FrontendContext'
 import { Button } from '@/components/ui/button'
@@ -15,6 +15,7 @@ const EditorContainer = () => {
     const { theme, systemTheme } = useTheme()
     const { id } = useParams<{ id: string }>();
     const [localContent, setLocalContent] = useState('');
+    const [openFile, setOpenFile] = useState<any>(null);
 
     const { isLoading, error, data } = db.useQuery({
         projects: {
@@ -29,19 +30,28 @@ const EditorContainer = () => {
     const { data:files, isLoading: isFilesLoading } = db.useQuery({ files: { $: { where: { projectId: id } } } });
     const currentlyOpen = files?.files?.find((file) => file.isOpen === true);
 
+    useEffect(() => {
+        if (currentlyOpen && currentlyOpen.content !== localContent) {
+            setOpenFile(currentlyOpen);
+            setLocalContent(currentlyOpen.content);
+        }
+    }, [currentlyOpen]);
+
     
     const latex = currentlyOpen?.content ?? '';
 
-    const debouncedUpdateDb = useDebounce((newCode: string) => {
-        // if (currentlyOpen?.id) {
-        //     db.transact([tx.files[currentlyOpen.id].update({ content: newCode })]);
-        // }
+    const debouncedUpdateDb = useDebounce((newCode: string, prevOpenFile: any) => {
+        if (prevOpenFile?.id) {
+            db.transact([tx.files[prevOpenFile.id].update({ content: newCode })]);
+        }
     }, 250);
 
     const handleCodeChange = useCallback((newCode: string) => {
-        setLocalContent(newCode);
-        debouncedUpdateDb(newCode);
-    }, [debouncedUpdateDb]);
+        if (newCode !== localContent) {
+            setLocalContent(newCode);
+            debouncedUpdateDb(newCode, openFile);
+        }
+    }, [debouncedUpdateDb, openFile, localContent]);
 
     if (isLoading || isFilesLoading) {
         return (
@@ -63,8 +73,8 @@ const EditorContainer = () => {
             </div>
             <CodeEditor
                 onChange={handleCodeChange}
-                value={latex}
-                key={theme || systemTheme}
+                value={localContent}
+                key={`${theme || systemTheme}-${openFile?.id}`}
             />
         </div>
     )
