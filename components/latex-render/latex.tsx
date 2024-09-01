19553ecb238d358ee74fd7893566b11fc67c8d66
Shelwin <shelwinsunga@gmail.com>
2024-09-01 01:04:31 -0500

    real app should have real URL

--- components/latex-render/latex.tsx
+++ components/latex-render/latex.tsx
@@ -16,6 +16,7 @@ import { useParams } from 'next/navigation'
 import { savePdfToStorage, savePreviewToStorage } from '@/lib/utils/db-utils';
 import '@ungap/with-resolvers';
 import { Download } from 'lucide-react';
+
 pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.mjs';
 
 
@@ -56,7 +57,7 @@ const LatexRenderer = () => {
         setError(null);
         setIsDocumentReady(false);
         try {
-            const response = await fetch('http://127.0.0.1:8000/', {
+            const response = await fetch('https://fastapi-production-6904.up.railway.app/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
