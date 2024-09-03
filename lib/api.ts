3a58b304804773c54b12c5023207969ae4327b4a
Shelwin <shelwinsunga@gmail.com>
2024-09-03 15:20:02 -0500

    make URL public

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,6 @@
 import { RAILWAY_ENDPOINT_URL } from '@/lib/constants'  
 export async function fetchPdf(latex: string) {
+  console.log(RAILWAY_ENDPOINT_URL)
     const response = await fetch(RAILWAY_ENDPOINT_URL, {
       method: 'POST',
       headers: {
--- lib/constants.ts
+++ lib/constants.ts
@@ -3,5 +3,5 @@ import { init } from '@instantdb/react'
 
 export const APP_ID = process.env.NEXT_PUBLIC_INSTANT_APP_ID as string
 export const db = init({ appId: APP_ID })
-export const RAILWAY_ENDPOINT_URL = process.env.RAILWAY_ENDPOINT_URL as string
+export const RAILWAY_ENDPOINT_URL = 'https://fastapi-production-6904.up.railway.app/';
 
