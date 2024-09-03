275a5c1d451279e64e7cb50d7af8b0e0b0046b60
Shelwin <shelwinsunga@gmail.com>
2024-09-02 22:50:14 -0500

    types

--- contexts/ProjectContext.tsx
+++ contexts/ProjectContext.tsx
@@ -1,13 +1,19 @@
-// contexts/ProjectContext.tsx
 import React, { createContext, useContext, ReactNode } from 'react';
 import { useProjectData, useProjectFiles } from '@/hooks/data';
 
-const ProjectContext = createContext<any>(null);
+// TODO: Add better types
+interface ProjectContextType {
+  project: any;
+  files: any;
+  isLoading: boolean;
+  error: any;
+}
+
+const ProjectContext = createContext<ProjectContextType | undefined>(undefined);
 
 export function ProjectProvider({ children, projectId }: { children: ReactNode, projectId: string }) {
   const { data: projectData, isLoading: projectIsLoading, error: projectError } = useProjectData(projectId);
   const { data: filesData, isLoading: filesIsLoading, error: filesError } = useProjectFiles(projectId);
-  console.log(filesData);
 
   return (
     <ProjectContext.Provider value={{ project: projectData?.projects[0], files: filesData?.files, isLoading: projectIsLoading || filesIsLoading, error: projectError || filesError }}>
