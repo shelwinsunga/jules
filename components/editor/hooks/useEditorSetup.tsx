631549eb54085c4b111130375456e2e7d0206831
Shelwin <shelwinsunga@gmail.com>
2024-09-23 10:10:16 -0500

    comment/uncomment command

--- components/editor/hooks/useEditorSetup.tsx
+++ components/editor/hooks/useEditorSetup.tsx
@@ -29,10 +29,38 @@ export function useEditorSetup(onChange: (value: string) => void, value: string)
     languages.register({ id: 'latex' });
     languages.setMonarchTokensProvider('latex', latex);
     
-
     monacoInstance.editor.setModelLanguage(editor.getModel()!, 'latex');
 
     editor.setValue(value)
+
+    editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.Slash, () => {
+      const selection = editor.getSelection()
+      if (selection) {
+        const model = editor.getModel()
+        if (model) {
+          const startLineNumber = selection.startLineNumber
+          const endLineNumber = selection.endLineNumber
+          const operations = []
+
+          for (let i = startLineNumber; i <= endLineNumber; i++) {
+            const lineContent = model.getLineContent(i)
+            if (lineContent.startsWith('%')) {
+              operations.push({
+                range: new monaco.Range(i, 1, i, 2),
+                text: '',
+              })
+            } else {
+              operations.push({
+                range: new monaco.Range(i, 1, i, 1),
+                text: '%',
+              })
+            }
+          }
+
+          model.pushEditOperations([], operations, () => null)
+        }
+      }
+    })
   }
 
   return { editorRef, handleEditorDidMount }
